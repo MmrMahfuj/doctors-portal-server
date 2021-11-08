c8dfa9867862e3238b0e3c93064cebd92358dbba
MmrMahfuj <mahfuj.mmr3@gmail.com>
2021-11-08 16:55:50 +0600

    removed locale string for date

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ async function run() {
         // GET API Appointment
         app.get('/appointment', verifyToken, async (req, res) => {
             const email = req.query.email;
-            const date = new Date(req.query.date).toLocaleDateString();
+            const date = req.query.date;
 
             const query = { email: email, date: date };
             const cursor = appointmentCollection.find(query);
